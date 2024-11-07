6fef3f1f42937c306e8fef4d83f1059561cdc798
eungbin <programmer97@naver.com>
2024-11-07 20:45:23 +0900

    Setting project for leaderboad

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, './dist'), // 결과물 경로
+    filename: 'bundle.js', // 결과물 파일명
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  devtool: 'eval-cheap-source-map',
+  devServer: {
+    hot: true,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    client: {
+      overlay: true,
+    },
+    port: 5500,
+    host: "localhost",
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        // use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(jpeg|jpg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new MiniCssExtractPlugin({ filename: 'app.css' }),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
+};
