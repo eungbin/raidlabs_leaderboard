d031f66cb94f7abf29572946784521a0b8f29096
eungbin <programmer97@naver.com>
2024-11-07 21:01:15 +0900

    Get dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 interface Props {}
 
 const App = ({  }: Props) => {
+  const [data, setData] = useState([]);
+  
+  useEffect(() => {
+    fetch(`https://gateway.pinata.cloud/ipfs/bafkreia2tigtk5kv5x6mptrscob7rwyvooyzte2j7luimkfssvm3m2zf54`)
+    .then(res => {
+      return res.json();
+    })
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }, []);
+
   return (
     <div className="container">
-      <h1>Hello, World!</h1>
+
     </div>
   );
 };
